f7a6b78c6a151a4dfeb78983eb527253dce6890e
Charlie Baxter and Caleb Stokka <caleb@>
2016-06-02 15:55:44 -0700

    create webpage that takes in triangle side lengths and gives you the type of triangle

---
+++ js/scripts.js
@@ -0,0 +1,27 @@
+$(document).ready(function() {
+  $("form").submit(function(event) {
+    event.preventDefault();
+
+    var firstSide = parseInt($("#side1").val());
+    var secondSide = parseInt($("#side2").val());
+    var thirdSide = parseInt($("#side3").val());
+
+    if (firstSide + secondSide <= thirdSide || firstSide + thirdSide <= secondSide || secondSide + thirdSide <= firstSide){
+      alert("This is not a triangle!");
+      }
+    else {
+
+      if (firstSide === secondSide && firstSide === thirdSide){
+        $("#result").text("This is an equilateral triangle");
+      }
+
+      else if (firstSide === secondSide || firstSide === thirdSide || secondSide === thirdSide){
+        $("#result").text("This is an isosceles triangle");
+      }
+
+      else if (firstSide != secondSide && firstSide != thirdSide && secondSide != thirdSide){
+        $("#result").text("This is a scalene triangle");
+      }
+    }
+  });
+});
